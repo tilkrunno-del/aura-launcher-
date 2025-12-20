231803dad2794db54af961afaf820f1b013803c3
tilkrunno-del <tilkrunno@gmail.com>
2025-12-20 16:59:51 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,22 @@
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    plugins {
+        id("com.android.application") version "8.3.2"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+    }
 }
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "AURALauncher"
+include(":app")
