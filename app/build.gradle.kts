b1a53ea5d22841e62d72b1aa054a281b9fd216f1
tilkrunno-del <tilkrunno@gmail.com>
2025-12-21 23:11:07 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,27 @@
 
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    namespace = "com.aura.launcher"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.aura.launcher"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "0.1"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+}
