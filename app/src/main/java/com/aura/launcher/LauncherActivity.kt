3848f05a1c274d62277f691c1aa4be0ecba0c82e
tilkrunno-del <tilkrunno@gmail.com>
2025-12-21 23:37:21 +0200

    Create LauncherActivity.kt

---
+++ app/src/main/java/com/aura/launcher/LauncherActivity.kt
@@ -0,0 +1,10 @@
+package com.aura.launcher
+
+import android.app.Activity
+import android.os.Bundle
+
+class LauncherActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
