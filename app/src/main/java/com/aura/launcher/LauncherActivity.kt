6032e9ad4b4a1fa19247ae39e2db6ca99220a287
Runno Tilk <tilkrunno@gmail.com>
2026-01-01 14:51:45 +0200

    Update LauncherActivity.kt
    push
    
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- app/src/main/java/com/aura/launcher/LauncherActivity.kt
+++ app/src/main/java/com/aura/launcher/LauncherActivity.kt
@@ -2,9 +2,20 @@ package com.aura.launcher
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 
 class LauncherActivity : Activity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val textView = TextView(this).apply {
+            text = "AURA Launcher is running"
+            textSize = 24f
+            gravity = Gravity.CENTER
+        }
+
+        setContentView(textView)
     }
 }
