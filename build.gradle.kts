5761a818b269bfa6f2f646f3acf71f8a7bde2b65
Runno Tilk <tilkrunno@gmail.com>
2026-01-03 01:08:45 +0200

    Update build.gradle.kts
    Define Android Gradle Plugin version
    
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
+
 plugins {
-    id("com.android.application")
-    kotlin("android")
+    id("com.android.application") version "8.1.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
