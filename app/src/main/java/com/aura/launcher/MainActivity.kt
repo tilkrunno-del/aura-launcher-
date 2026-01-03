d882c6ba4dbf2841addb2ae246191259d32b1e41
Runno Tilk <tilkrunno@gmail.com>
2026-01-03 19:29:59 +0200

    Update MainActivity.kt
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- app/src/main/java/com/aura/launcher/MainActivity.kt
+++ app/src/main/java/com/aura/launcher/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.aura.launcher
 
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val tv = TextView(this).apply {
+            text = "AURA Launcher"
+            gravity = Gravity.CENTER
+            textSize = 22f
+        }
+
+        setContentView(tv)
     }
 }
