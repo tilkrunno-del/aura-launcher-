8ab1961c9d06eea959ea1a92c14b9108a1f52549
Runno Tilk <tilkrunno@gmail.com>
2026-01-04 04:48:32 +0200

    Update MainActivity.kt
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- app/src/main/java/com/aura/launcher/MainActivity.kt
+++ app/src/main/java/com/aura/launcher/MainActivity.kt
@@ -7,13 +7,27 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-    val btn = findViewById<Button>(R.id.btnOpenApps)
-    btn.setOnClickListener {
-      startActivity(Intent(this, AppsActivity::class.java))
+        val btn = findViewById<Button>(R.id.btnOpenApps)
+        btn.setOnClickListener {
+            startActivity(Intent(this, AppsActivity::class.java))
+        }
     }
-  }
+
+    // Kuna launchMode="singleTask", siis HOME vajutus võib tulla siia ilma uut activity't tegemata
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent != null) {
+            setIntent(intent)
+        }
+    }
+
+    // BACK ei tohi launcherit “kinni panna” – liigume lihtsalt taustale
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        moveTaskToBack(true)
+    }
 }
