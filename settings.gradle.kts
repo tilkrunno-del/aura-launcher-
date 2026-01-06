13aa9ceb7defbd270f957d4c17e79e3241aa199b
Runno Tilk <tilkrunno@gmail.com>
2026-01-06 16:50:20 +0200

    Update settings.gradle.kts
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,16 @@
 pluginManagement {
     repositories {
         google()
-        maven { url = uri("https://repo1.maven.org/maven2") }
+        mavenCentral()
         gradlePluginPortal()
     }
 }
-
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
-        maven { url = uri("https://repo1.maven.org/maven2") }
+        mavenCentral()
     }
 }
-
 rootProject.name = "aura-launcher-"
 include(":app")
