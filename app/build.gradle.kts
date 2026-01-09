b61426f940cd702c5dce99fed2ab13020024fce5
Runno Tilk <tilkrunno@gmail.com>
2026-01-09 18:00:05 +0200

    Update build.gradle.kts
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,35 +23,20 @@ android {
                 "proguard-rules.pro"
             )
         }
-        debug {
-            isMinifyEnabled = false
-        }
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-
     kotlinOptions {
         jvmTarget = "17"
-    }
-
-    buildFeatures {
-        viewBinding = true
     }
 }
 
 dependencies {
-
-    // Android core
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
-
-    // UI
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    // RecyclerView (vajalik launcherile)
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.android.material:material:1.12.0")
 }
