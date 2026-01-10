76376fb1645dd56a8fc9f6c1ec43ec348f5b7839
Runno Tilk <tilkrunno@gmail.com>
2026-01-10 19:43:46 +0200

    Update AppActionsBottomSheet.kt
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- app/src/main/java/com/aura/launcher/AppActionsBottomSheet.kt
+++ app/src/main/java/com/aura/launcher/AppActionsBottomSheet.kt
@@ -3,26 +3,35 @@ package com.aura.launcher
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.TextView
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class AppActionsBottomSheet(
     context: Context,
-    private val app: AppInfo
+    private val app: AppInfo,
+    private val onOpen: (AppInfo) -> Unit
 ) {
 
     private val dialog = BottomSheetDialog(context)
 
-    fun show() {
-        val view = LayoutInflater.from(dialog.context)
-            .inflate(R.layout.sheet_app_actions, null)
-
-        view.findViewById<View>(R.id.action_open)?.setOnClickListener {
-            LauncherUtils.launchApp(dialog.context, app)
-            dialog.dismiss()
-        }
+    init {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.sheet_app_actions, null, false)
 
         dialog.setContentView(view)
+
+        val openAction = view.findViewById<TextView>(R.id.action_open)
+        val title = view.findViewById<TextView>(R.id.appTitle)
+
+        title.text = app.label
+
+        openAction.setOnClickListener {
+            dialog.dismiss()
+            onOpen(app)
+        }
+    }
+
+    fun show() {
         dialog.show()
     }
 }
