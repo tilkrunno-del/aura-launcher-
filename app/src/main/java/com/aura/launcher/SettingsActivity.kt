8b42d59f01862b682d89f980e283bfb236f719c9
Runno Tilk <tilkrunno@gmail.com>
2026-01-10 20:11:13 +0200

    Update SettingsActivity.kt
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- app/src/main/java/com/aura/launcher/SettingsActivity.kt
+++ app/src/main/java/com/aura/launcher/SettingsActivity.kt
@@ -1,68 +1,38 @@
 package com.aura.launcher
 
 import android.os.Bundle
-import android.widget.RadioButton
-import android.widget.RadioGroup
-import android.widget.SeekBar
-import android.widget.TextView
+import android.widget.Button
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        ThemePrefs.applyNightMode(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val themeGroup = findViewById<RadioGroup>(R.id.themeGroup)
-        val radioSystem = findViewById<RadioButton>(R.id.radioSystem)
-        val radioLight = findViewById<RadioButton>(R.id.radioLight)
-        val radioDark = findViewById<RadioButton>(R.id.radioDark)
+        LauncherSettings.applyNightMode(this)
 
-        val columnsValue = findViewById<TextView>(R.id.columnsValue)
-        val columnsSeek = findViewById<SeekBar>(R.id.columnsSeek)
-
-        val animSwitch = findViewById<SwitchCompat>(R.id.animSwitch)
-
-        // --- init values ---
-        when (ThemePrefs.getThemeMode(this)) {
-            ThemePrefs.THEME_LIGHT -> radioLight.isChecked = true
-            ThemePrefs.THEME_DARK -> radioDark.isChecked = true
-            else -> radioSystem.isChecked = true
-        }
-
-        val currentSpan = ThemePrefs.getSpanCount(this)
-        columnsValue.text = currentSpan.toString()
-        columnsSeek.max = 2              // 0..2 => 3..5
-        columnsSeek.progress = currentSpan - 3
-
-        animSwitch.isChecked = ThemePrefs.isAnimEnabled(this)
-
-        // --- listeners ---
-        themeGroup.setOnCheckedChangeListener { _, checkedId ->
-            val mode = when (checkedId) {
-                R.id.radioLight -> ThemePrefs.THEME_LIGHT
-                R.id.radioDark -> ThemePrefs.THEME_DARK
-                else -> ThemePrefs.THEME_SYSTEM
-            }
-            ThemePrefs.setThemeMode(this, mode)
-            ThemePrefs.applyNightMode(this)
+        findViewById<Button>(R.id.btnThemeLight).setOnClickListener {
+            LauncherSettings.setTheme(this, LauncherSettings.THEME_LIGHT)
             recreate()
         }
 
-        columnsSeek.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                val span = 3 + progress
-                columnsValue.text = span.toString()
-                ThemePrefs.setSpanCount(this@SettingsActivity, span)
-            }
-            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
-        })
+        findViewById<Button>(R.id.btnThemeDark).setOnClickListener {
+            LauncherSettings.setTheme(this, LauncherSettings.THEME_DARK)
+            recreate()
+        }
+
+        findViewById<Button>(R.id.btnThemeSystem).setOnClickListener {
+            LauncherSettings.setTheme(this, LauncherSettings.THEME_SYSTEM)
+            recreate()
+        }
+
+        val animSwitch = findViewById<Switch>(R.id.switchAnimations)
+        animSwitch.isChecked = LauncherSettings.isAnimEnabled(this)
 
         animSwitch.setOnCheckedChangeListener { _, isChecked ->
-            ThemePrefs.setAnimEnabled(this, isChecked)
+            LauncherSettings.setAnimEnabled(this, isChecked)
         }
     }
 }
