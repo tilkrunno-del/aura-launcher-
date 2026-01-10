59e967a2f0e81144913f78de8bae56028404a871
Runno Tilk <tilkrunno@gmail.com>
2026-01-10 18:39:22 +0200

    Update AppInfo.kt
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- app/src/main/java/com/aura/launcher/AppInfo.kt
+++ app/src/main/java/com/aura/launcher/AppInfo.kt
@@ -1,14 +1,10 @@
 package com.aura.launcher
 
 import android.graphics.drawable.Drawable
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class AppInfo(
     val packageName: String,
     val className: String,
     val label: String,
-    @kotlinx.parcelize.IgnoredOnParcel
-    val icon: Drawable? = null
-) : Parcelable
+    val icon: Drawable
+)
