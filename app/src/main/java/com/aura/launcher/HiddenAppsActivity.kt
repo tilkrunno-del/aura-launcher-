0f84013f034432c99f84ccf022a503e4029cf042
Runno Tilk <tilkrunno@gmail.com>
2026-01-11 18:36:12 +0200

    Update HiddenAppsActivity.kt
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- app/src/main/java/com/aura/launcher/HiddenAppsActivity.kt
+++ app/src/main/java/com/aura/launcher/HiddenAppsActivity.kt
@@ -2,26 +2,10 @@ package com.aura.launcher
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class HiddenAppsActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_apps)
-
-        val recycler = findViewById<RecyclerView>(R.id.appsRecyclerView)
-        recycler.layoutManager = GridLayoutManager(this, 4)
-
-        val hiddenApps = LauncherUtils.getHiddenApps(this)
-
-        recycler.adapter = AppsAdapter(
-            apps = hiddenApps,
-            onClick = { app ->
-                LauncherUtils.launchApp(this, app)
-            },
-            onLongPress = { _, _ -> }
-        )
+        finish() // ajutiselt välja
     }
 }
