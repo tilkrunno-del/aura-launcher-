b1c5b64a0486f1c7476d27af6fb6d68e4fc5fe06
Runno Tilk <tilkrunno@gmail.com>
2026-01-11 18:37:41 +0200

    Update SettingsActivity.kt
    Signed-off-by: Runno Tilk <tilkrunno@gmail.com>

--- app/src/main/java/com/aura/launcher/SettingsActivity.kt
+++ app/src/main/java/com/aura/launcher/SettingsActivity.kt
@@ -1,38 +1,11 @@
 package com.aura.launcher
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
-
-        LauncherSettings.applyNightMode(this)
-
-        findViewById<Button>(R.id.btnThemeLight).setOnClickListener {
-            LauncherSettings.setTheme(this, LauncherSettings.THEME_LIGHT)
-            recreate()
-        }
-
-        findViewById<Button>(R.id.btnThemeDark).setOnClickListener {
-            LauncherSettings.setTheme(this, LauncherSettings.THEME_DARK)
-            recreate()
-        }
-
-        findViewById<Button>(R.id.btnThemeSystem).setOnClickListener {
-            LauncherSettings.setTheme(this, LauncherSettings.THEME_SYSTEM)
-            recreate()
-        }
-
-        val animSwitch = findViewById<Switch>(R.id.switchAnimations)
-        animSwitch.isChecked = LauncherSettings.isAnimEnabled(this)
-
-        animSwitch.setOnCheckedChangeListener { _, isChecked ->
-            LauncherSettings.setAnimEnabled(this, isChecked)
-        }
+        finish() // ajutiselt välja
     }
 }
